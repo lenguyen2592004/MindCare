29f997c898d004404d32132de868f0627bfdcb7c
lenguyen2592004 <120765515+lenguyen2592004@users.noreply.github.com>
2023-05-25 21:20:43 +0700

    Add files via upload

---
+++ assest/assets/js/app.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+$(document).ready(function () {
+    $('#eye').click(function () {
+        $(this).toggleClass('open');
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if ($(this).hasClass('open')) {
+            $(this).prev().attr('type', 'text');
+        } else {
+            $(this).prev().attr('type', 'password');
+        }
+    });
+});
